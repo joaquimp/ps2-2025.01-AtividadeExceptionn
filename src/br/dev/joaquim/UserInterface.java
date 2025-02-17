ac791c87b9ddcb4b89a404cd61537be9cb91d46b
gustavonc05 <gunetto24@gmail.com>
2025-02-17 23:09:57 +0000

    Correção da atividade

--- src/br/dev/joaquim/UserInterface.java
+++ src/br/dev/joaquim/UserInterface.java
@@ -61,18 +61,80 @@ public class UserInterface {
         }
     }
 
+    /**
+        * Realiza um deposito no saldo ({@link #balance}) da conta, a partir de um valor informado ao usuário.
+        * 
+        * Este método solicita ao usuário o valor a ser depositado e verifica se o valor
+        * é negativo. Caso seja, uma exceção {@link IllegalArgumentException} será lançada.
+        * Caso o valor seja válido, o depósito será realizado na conta.
+        *
+        * @var value Variável que armazena o valor que o usuário quer depositar.
+        * @var balance Variável que armazena o saldo atual da conta.
+        *
+        * @throws IllegalArgumentException Se o valor a ser depositado for negativo.
+    */
     private void deposit() {
         System.out.print("\nInforme o valor a ser depositado: ");
         double value = readValue();
-        account.deposit(value);
-        System.out.println("Desposito realizado com sucesso.");
+
+        // Criando um try com catch para gerar a exceção caso o valor a depositar seja negativo
+        try{
+            if (value<0){
+                throw new IllegalArgumentException("Não é possível depositar um valor negativo!");
+            }
+            else{
+                account.deposit(value);
+                System.out.println("Desposito realizado com sucesso.");
+            }
+        }
+        catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }      
     }
 
+    /**
+        * Realiza uma retirada no saldo ({@link #balance}) da conta, a partir de um valor informado ao usuário.
+        * 
+        * Este método solicita ao usuário o valor a ser sacado e verifica se o valor
+        * é negativo ou insuficiente. Caso seja negativo, uma exceção {@link IllegalArgumentException} 
+        * será lançada, de outra forma, caso seja um valor insuficiente para sacar, uma exceção {@link InsufficientFundsException} será lançada.
+        * Caso o valor seja válido, o saque será realizado na conta.
+        *
+        * @var value Variável que armazena o valor que o usuário quer sacar.
+        * @var balance Variável que armazena o saldo atual da conta, utilizado para verificar se o saque pode ser realizado.
+        *
+        * @throws IllegalArgumentException Se o valor a ser sacado for negativo.
+        * @throws InsufficientFundsException Se o valor a ser sacado for maior do que o valor no saldo do usuário.
+    */
     private void withdraw() {
         System.out.print("\nInforme o valor a ser sacado: ");
+
+        // Valor a sacar
         double value = readValue();
-        account.withdraw(value); // pode dar problema
-        System.out.println("Saque realizado com sucesso");
+
+        // Valor atual do saldo da conta
+        double balance = account.getBalance();
+
+        // Criado um try com duas verificações (IF) que caso não atenda a tal requisito, gera uma exceção que acaba não encerrando o programa e gerando uma mensagem
+        try{
+            if (value<0){
+                throw new IllegalArgumentException("Não é possível sacar um valor negativo!");
+            }
+            // Aqui foi citado uma Exception ESPECÍFICA criada por mim
+            else if(value>balance){
+                throw new InsufficientFundsException ("\nSaldo atual =  "+ balance + "\nSaldo que você deseja sacar = " + value + "\nVocê não tem saldo disponível");
+            }
+            else{
+            account.withdraw(value); 
+            System.out.println("Saque realizado com sucesso");
+            }
+        }
+        catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+        catch(InsufficientFundsException e){
+            System.out.println(e.getMessage());
+        }
     }
 
     private int readOption() {
